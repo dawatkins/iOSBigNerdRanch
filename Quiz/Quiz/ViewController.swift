883dcbaa35fb882543f062d763dd2dbc0bc1d978
David <dawatkins16@gmail.com>
2020-12-09 16:04:53 -0500

    Initial Commit for iOS BigNerdRanch

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by David Watkins on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
+}
+
